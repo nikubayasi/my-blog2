1192ce25ad4978d108b2f735bc1d0686cc85b715
nikubayasi <29bayasi@gmail.com>
2025-02-15 09:04:55 +0900

    update

--- src/lib/actions/user.js
+++ src/lib/actions/user.js
@@ -1,4 +1,4 @@
-import User from '../models/user.model';
+import User from '../models/user.models';
 
 import { connect } from '../mongodb/mongoose';
 
--- src/lib/models/user.models.js
+++ src/lib/models/user.models.js
@@ -2,44 +2,41 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
-    clerkId: {
+    clerkId:{
       type: String,
       required: true,
       unique: true,
     },
-
-    email: {
+    email:{
       type: String,
       required: true,
       unique: true,
     },
-
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    username: {
+    firstName:{
       type: String,
       required: true,
       unique: true,
     },
-
-    profilePicture: {
-      type: String,
-      required: false,
+    lastName:{
+      type:String,
+      required: true,
+      unique: true,
     },
-
-    isAdmin: {
-      type: Boolean,
+    username:{
+      type:String,
+      required: true,
+      unique: true,
+    },
+    profilePicture:{
+      type:String,
       default: false,
     },
-  },
-  { timestamps: true }
+    isAdmin:{
+      type:Boolean,
+      default: false,
+    }
+  },{timestamps: true}
 );
 
-const User = mongoose.models.User || mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model('User',userSchema);
 export default User;
